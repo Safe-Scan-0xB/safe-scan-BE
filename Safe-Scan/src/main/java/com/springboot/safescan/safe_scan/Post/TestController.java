28bed1d5c658962e7b1ac83712f727cfaa28127d
최희선 <todaysunny612@gmail.com>
2025-11-08 01:50:00 +0900

    [fix] Safe-Scan 폴더를 일반 폴더로 포함

---
+++ Safe-Scan/src/main/java/com/springboot/safescan/safe_scan/Post/TestController.java
@@ -0,0 +1,5 @@
+package com.springboot.safescan.safe_scan.Post;
+
+public class TestController {
+    //하실 때 지워서 사용하세요
+}
---
+++ Safe-Scan/src/test/java/com/springboot/safescan/SafeScanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.safescan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeScanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
