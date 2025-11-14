76969b01111ed8e5bb14e3f927afa1da41d7a907
mimging <6636jj@naver.com>
2025-11-14 15:37:30 +0900

    signup 수정

--- Safe-Scan/src/main/java/com/springboot/safescan/service/AuthService.java
+++ Safe-Scan/src/main/java/com/springboot/safescan/service/AuthService.java
@@ -13,6 +13,22 @@ public class AuthService {
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
 
+    public String signup(String userId, String password) {
+
+        // 1) 중복 아이디 체크
+        if (userRepository.findByUserId(userId).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
+        }
+
+        // 2) 저장
+        User user = new User();
+        user.setUserId(userId);
+        user.setPassword(password);
+
+        userRepository.save(user);
+        return "회원가입 완료";
+    }
+
     public String login(String userId, String password) {
         User user = userRepository.findByUserId(userId)
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 회원"));
@@ -25,13 +41,7 @@ public class AuthService {
         return jwtTokenProvider.createToken(userId);
     }
 
-    public String signup(String userId, String password) {
-        User user = new User();
-        user.setUserId(userId);
-        user.setPassword(password);
-        userRepository.save(user);
-        return "회원가입 완료";
-    }
+
 }
 
 
