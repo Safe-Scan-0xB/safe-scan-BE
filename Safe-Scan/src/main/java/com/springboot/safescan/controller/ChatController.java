c3522612190a2e47309a0c1d024e57ce007f7656
suyeon <109880402+ssuisig@users.noreply.github.com>
2025-11-19 23:15:26 +0900

    [feat] 24시간 db저장 기능 구현

--- Safe-Scan/src/main/java/com/springboot/safescan/controller/ChatController.java
+++ Safe-Scan/src/main/java/com/springboot/safescan/controller/ChatController.java
@@ -15,21 +15,15 @@ public class ChatController {
 
     private final ChatService chatService;
 
-    // 1) 지난 24시간 대화 조회
+    // 1) 지난 24시간 대화 조회 (커서 + size 기반)
     @GetMapping
-<<<<<<< HEAD
     public ChatHistoryResponse getHistory(
             Authentication auth,
             @RequestParam(required = false) Long cursor,
-            @RequestParam(defaultValue = "10") int size)
-    {
+            @RequestParam(defaultValue = "10") int size
+    ) {
         String userId = (String) auth.getPrincipal();
         return chatService.getHistory(userId, cursor, size);
-=======
-    public ChatHistoryResponse getHistory(Authentication auth) {
-        String userId = (String) auth.getPrincipal();
-        return chatService.getHistory(userId);
->>>>>>> 37a789065291445120663a8a557d27bd900e9c48
     }
 
     // 2) 챗봇에게 질문 → 답변 생성
